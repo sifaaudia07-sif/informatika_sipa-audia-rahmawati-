f7966113d86e9607943a9b96df79d8a529108ed8
sifaaudia07-sif <sifaaudia07@gmail.com>
2025-08-04 12:56:52 +0700

    Add files via upload

---
+++ tugas 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "nama    : SIPA audia rahmawati" << endl;
+    cout << "kelas   : X IPS" << endl;
+    cout << "sekolah : MAS AL-Manshuriyah" << endl;
+    cout << "alamat  : kp. Bojong Sari bunijaya" << endl;
+    cout << "umur    : 15 tahun" << endl;
+    return 0;
+}
