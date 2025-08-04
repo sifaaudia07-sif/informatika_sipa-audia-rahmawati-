1db61fdb7f5a16c5d15c22cddf2c98692c906e53
sifaaudia07-sif <sifaaudia07@gmail.com>
2025-08-04 17:42:01 +0700

    Add files via upload

---
+++ perhitungan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "nama     : SIPA audia rahmawati" << endl;
+    cout << "kelas    : X IPS" << endl;
+    cout << "sekolah  : MAS AL-Manshuriyah" << endl;
+    cout << "alamat   : kp. Bojong Sari bunijaya" << endl;
+    cout << "umur     : 15 tahun" << endl;
+    cout << "aplikasi : coding c++" << endl;
+    int bil1, bil2, hasil;
+    cout << "silahkan masukan bil 1" << endl;
+    cin >> bil1 ;
+    cout << "silahkan masukan bil 2" << endl;
+    cin >> bil2;
+    hasil = bil1 + bil2;
+    cout << hasil;
+    return 0;
+}
